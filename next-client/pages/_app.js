af789171c9e3f73e3bc47e168f5f665cc2c56443
jjjj88521 <jjjj88521@gmail.com>
2023-09-21 01:18:49 +0800

    0921 header移出navbar元件

--- next-client/pages/_app.js
+++ next-client/pages/_app.js
@@ -6,7 +6,7 @@ export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   // 頁面載入引入 bootstrap
   useEffect(() => {
-    import('bootstrap/dist/js/bootstrap')
+    import('bootstrap/dist/js/bootstrap.bundle.min.js')
   }, [])
   const getLayout =
     Component.getLayout ||
