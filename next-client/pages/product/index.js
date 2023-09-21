52e0a6065f6057e21619d84e6cba599be0b925bb
Larry <trumpet.0417@gmail.com>
2023-09-21 17:00:01 +0800

    0921test

--- next-client/pages/product/index.js
+++ next-client/pages/product/index.js
@@ -1,5 +1,94 @@
 import React from 'react'
+import styles from './product.module.css'
+
+// <div className="container">
+// <div className={styles['banner']}>
 
 export default function ProductIndex() {
-  return <div>ProductIndex</div>
+  return (
+    <>
+      <div className="container">
+        <div className={styles['banner']}>
+          <img
+            src="/images/testBanner.jpg"
+            alt="預留位子先不放圖"
+            className="img-fluid"
+          />
+          <h1 className={`z-3 position-absolute ${styles['display1']}`}>
+            鍵盤套件
+          </h1>
+        </div>
+        <div className="cate-Nav">
+          <div className="accordion" id="accordionPanelsStayOpenExample">
+            <div className="accordion-item">
+              <h2 className="accordion-header">
+                <button
+                  className="accordion-button"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#panelsStayOpen-collapseOne"
+                  aria-expanded="true"
+                  aria-controls="panelsStayOpen-collapseOne"
+                >
+                  Accordion Item #1
+                </button>
+              </h2>
+              <div
+                id="panelsStayOpen-collapseOne"
+                className="accordion-collapse collapse show"
+              >
+                <div className="accordion-body">
+                  This is the third accordion body.
+                </div>
+              </div>
+            </div>
+            <div className="accordion-item">
+              <h2 className="accordion-header">
+                <button
+                  className="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#panelsStayOpen-collapseTwo"
+                  aria-expanded="false"
+                  aria-controls="panelsStayOpen-collapseTwo"
+                >
+                  Accordion Item #2
+                </button>
+              </h2>
+              <div
+                id="panelsStayOpen-collapseTwo"
+                className="accordion-collapse collapse"
+              >
+                <div className="accordion-body">
+                  This is the third accordion body.
+                </div>
+              </div>
+            </div>
+            <div className="accordion-item">
+              <h2 className="accordion-header">
+                <button
+                  className="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#panelsStayOpen-collapseThree"
+                  aria-expanded="false"
+                  aria-controls="panelsStayOpen-collapseThree"
+                >
+                  Accordion Item #3
+                </button>
+              </h2>
+              <div
+                id="panelsStayOpen-collapseThree"
+                className="accordion-collapse collapse"
+              >
+                <div className="accordion-body">
+                  This is the third accordion body.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
