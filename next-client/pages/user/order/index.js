944ee935a5bbf55b01763a13d500beb85b129e74
ning <d825097@gmail.com>
2023-09-22 00:04:24 +0800

    0922 訂單列表手機版

--- next-client/pages/user/order/index.js
+++ next-client/pages/user/order/index.js
@@ -4,34 +4,34 @@ import style from '@/styles/order.module.scss'
 export default function Order() {
   return (
     <>
-      <div className="container">
-        <table className="table">
-          <thead className="">
-            <tr className="">
-              <th className="bg-primary"></th>
-              <th className="bg-primary text-white">訂單編號</th>
-              <th className="bg-primary text-white">訂單日期</th>
-              <th className="bg-primary text-white">訂單金額</th>
-              <th className="bg-primary text-white"></th>
+      <div className='container'>
+        <table className='table table-desktop'>
+          <thead className=''>
+            <tr className=''>
+              <th className='bg-primary'></th>
+              <th className='bg-primary text-white'>訂單編號</th>
+              <th className='bg-primary text-white'>訂單日期</th>
+              <th className='bg-primary text-white'>訂單金額</th>
+              <th className='bg-primary text-white'></th>
             </tr>
           </thead>
           <tbody>
-            <tr className="">
-              <td className="">1</td>
-              <td className="">P00001</td>
-              <td className="">2023/08/15</td>
-              <td className="">1800</td>
-              <td className="">
-                <button className="btn btn-primary text-light">查看</button>
+            <tr className=''>
+              <td className=''>1</td>
+              <td className=''>P00001</td>
+              <td className=''>2023/08/15</td>
+              <td className=''>1800</td>
+              <td className=''>
+                <button className='btn btn-primary text-light'>查看</button>
               </td>
             </tr>
-            <tr className="">
-              <td className="">2</td>
-              <td className="">G00002</td>
-              <td className="">2023/08/16</td>
-              <td className="">800</td>
-              <td className="">
-                <button className="btn btn-primary text-light">查看</button>
+            <tr className=''>
+              <td className=''>2</td>
+              <td className=''>G00002</td>
+              <td className=''>2023/08/16</td>
+              <td className=''>800</td>
+              <td className=''>
+                <button className='btn btn-primary text-light'>查看</button>
               </td>
             </tr>
             {/* <tr>
@@ -40,27 +40,18 @@ export default function Order() {
                 <td>2023/08/17</td>
                 <td>1000</td>
                 <td>
-                  <button className="btn btn-primary text-light">查看</button>
+                  <button className='btn btn-primary text-light'>查看</button>
                 </td>
               </tr> */}
           </tbody>
         </table>
-        <table>
-          {/* <thead>
-              <tr>
-                <th>1</th>
-                <th>訂單編號</th>
-                <th>訂單日期</th>
-                <th>訂單金額</th>
-                <th></th>
-              </tr>
-            </thead> */}
-          <tbody>
+        
+        <table className='table table-bordered table-mobile'>
+          <tbody className=''>
             <tr>
-              <tr className="" colspan="2">
+              <th className=''>
                 1
-              </tr>
-              <td></td>
+              </th>
             </tr>
             <tr>
               <th>訂單編號</th>
@@ -77,11 +68,11 @@ export default function Order() {
             <tr>
               <th></th>
               <td>
-                <button className="btn btn-primary text-light">查看</button>
+                <button className='btn btn-primary text-light'>查看</button>
               </td>
             </tr>
           </tbody>
-        </table>
+        </table>      
       </div>
     </>
   )
