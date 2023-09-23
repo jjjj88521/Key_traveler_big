2411fcc72ddf71a490683aff0006c5dc49fdccef
Larry <trumpet.0417@gmail.com>
2023-09-23 17:06:31 +0800

    商品列表頁改用grid，accordion完成一半吧？？？

--- next-client/pages/product/index.js
+++ next-client/pages/product/index.js
@@ -13,349 +13,157 @@ export default function ProductIndex() {
         <h1 className={`text-primary ${styles['display1']}`}>鍵盤套件</h1>
       </div>
 
-      <div className="container pt-5 d-flex gap-3">
-        <div className={styles['cateNav']}>
-          <div className="accordion" id="accordionPanelsStayOpenExample">
-            <div className="accordion-item">
-              <h2 className="accordion-header">
-                <button
-                  className="accordion-button"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#panelsStayOpen-collapseOne"
-                  aria-expanded="true"
-                  aria-controls="panelsStayOpen-collapseOne"
+      <div className="container pt-5 pb-3">
+        <div className="row">
+          <div className="col-3">
+            <div className="accordion" id="accordionPanelsStayOpenExample">
+              <div className="accordion-item">
+                <h2 className="accordion-header">
+                  <button
+                    className="accordion-button"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#panelsStayOpen-collapseOne"
+                    aria-expanded="true"
+                    aria-controls="panelsStayOpen-collapseOne"
+                  >
+                    套件
+                  </button>
+                </h2>
+                <div
+                  id="panelsStayOpen-collapseOne"
+                  className="accordion-collapse collapse show"
                 >
-                  Accordion Item #1
-                </button>
-              </h2>
-              <div
-                id="panelsStayOpen-collapseOne"
-                className="accordion-collapse collapse show"
-              >
-                <div className="accordion-body">
-                  This is the third accordion body.
+                  <div className="accordion-body">
+                    <ul>
+                      <li>60%</li>
+                      <li>65%</li>
+                      <li>75%</li>
+                      <li>80%</li>
+                      <li>98%</li>
+                      <li>100%</li>
+                      <li>Num Pad</li>
+                    </ul>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="accordion-item">
-              <h2 className="accordion-header">
-                <button
-                  className="accordion-button collapsed"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#panelsStayOpen-collapseTwo"
-                  aria-expanded="false"
-                  aria-controls="panelsStayOpen-collapseTwo"
+              <div className="accordion-item">
+                <h2 className="accordion-header">
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#panelsStayOpen-collapseTwo"
+                    aria-expanded="false"
+                    aria-controls="panelsStayOpen-collapseTwo"
+                  >
+                    Accordion Item #2
+                  </button>
+                </h2>
+                <div
+                  id="panelsStayOpen-collapseTwo"
+                  className="accordion-collapse collapse"
                 >
-                  Accordion Item #2
-                </button>
-              </h2>
-              <div
-                id="panelsStayOpen-collapseTwo"
-                className="accordion-collapse collapse"
-              >
-                <div className="accordion-body">
-                  This is the third accordion body.
+                  <div className="accordion-body">
+                    This is the third accordion body.
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="accordion-item">
-              <h2 className="accordion-header">
-                <button
-                  className="accordion-button collapsed"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#panelsStayOpen-collapseThree"
-                  aria-expanded="false"
-                  aria-controls="panelsStayOpen-collapseThree"
+              <div className="accordion-item">
+                <h2 className="accordion-header">
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#panelsStayOpen-collapseThree"
+                    aria-expanded="false"
+                    aria-controls="panelsStayOpen-collapseThree"
+                  >
+                    Accordion Item #3
+                  </button>
+                </h2>
+                <div
+                  id="panelsStayOpen-collapseThree"
+                  className="accordion-collapse collapse"
                 >
-                  Accordion Item #3
-                </button>
-              </h2>
-              <div
-                id="panelsStayOpen-collapseThree"
-                className="accordion-collapse collapse"
-              >
-                <div className="accordion-body">
-                  This is the third accordion body.
+                  <div className="accordion-body">
+                    This is the third accordion body.
+                  </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className={styles['produtListGroup']}>
-          <div className="d-flex align-items-center flex-row-reverse mb-3">
-            <div className={`bg-primary-subtle ${styles['sortBtn']}`}>
-              <p className="fs-6">排序</p>
-              <div className="dropdown">
-                <button
-                  className="btn btn-light dropdown-toggle text-dark"
-                  type="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  預設
-                </button>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      價錢：由低到高
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      價錢：由高到低
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      上架日期：由新到舊
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      上架日期：由舊到新
-                    </a>
-                  </li>
-                </ul>
               </div>
             </div>
           </div>
 
-          {/* product list & card group  */}
-          <div className="row row-cols-2 row-cols-md-3 g-4">
-            <div class="col">
-              <div className="card">
-                <img
-                  src="/images/card.png"
-                  className="card-img-top"
-                  alt="..."
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the cards content.
-                  </p>
-                  <a href="#" className="btn btn-primary">
-                    Go somewhere
-                  </a>
+          {/* sort btn & card group */}
+          <div className="col-9">
+            <div className="d-flex justify-content-end align-items-center mb-3">
+              <div className={`bg-primary-subtle ${styles['sortBtn']}`}>
+                <p className="fs-6">排序</p>
+                <div className="dropdown">
+                  <button
+                    className="btn btn-light dropdown-toggle text-dark"
+                    type="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    預設
+                  </button>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        價錢：由低到高
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        價錢：由高到低
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        上架日期：由新到舊
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        上架日期：由舊到新
+                      </a>
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
-            <div class="col">
-              <div className="card">
-                <img
-                  src="/images/card.png"
-                  className="card-img-top"
-                  alt="..."
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the cards content.
-                  </p>
-                  <a href="#" className="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div className="card">
-                <img
-                  src="/images/card.png"
-                  className="card-img-top"
-                  alt="..."
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the cards content.
-                  </p>
-                  <a href="#" className="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div className="card">
-                <img
-                  src="/images/card.png"
-                  className="card-img-top"
-                  alt="..."
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the cards content.
-                  </p>
-                  <a href="#" className="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div className="card">
-                <img
-                  src="/images/card.png"
-                  className="card-img-top"
-                  alt="..."
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the cards content.
-                  </p>
-                  <a href="#" className="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div className="card">
-                <img
-                  src="/images/card.png"
-                  className="card-img-top"
-                  alt="..."
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the cards content.
-                  </p>
-                  <a href="#" className="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div className="card">
-                <img
-                  src="/images/card.png"
-                  className="card-img-top"
-                  alt="..."
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the cards content.
-                  </p>
-                  <a href="#" className="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div className="card">
-                <img
-                  src="/images/card.png"
-                  className="card-img-top"
-                  alt="..."
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the cards content.
-                  </p>
-                  <a href="#" className="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div className="card">
-                <img
-                  src="/images/card.png"
-                  className="card-img-top"
-                  alt="..."
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the cards content.
-                  </p>
-                  <a href="#" className="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div className="card">
-                <img
-                  src="/images/card.png"
-                  className="card-img-top"
-                  alt="..."
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the cards content.
-                  </p>
-                  <a href="#" className="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div className="card">
-                <img
-                  src="/images/card.png"
-                  className="card-img-top"
-                  alt="..."
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the cards content.
-                  </p>
-                  <a href="#" className="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div className="card">
-                <img
-                  src="/images/card.png"
-                  className="card-img-top"
-                  alt="..."
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the cards content.
-                  </p>
-                  <a href="#" className="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-              </div>
+
+            {/* product list & card group  */}
+            <div className="d-flex justify-content-end row row-cols-2 row-cols-md-3 g-4">
+              {Array(12)
+                .fill()
+                .map((_, index) => (
+                  <div className="col " key={index}>
+                    <div className="card">
+                      <img
+                        src="/images/card.png"
+                        className="card-img-top"
+                        alt="..."
+                      />
+                      <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">
+                          Some quick example text to build on the card title and
+                          make up the bulk of the card's content.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
             </div>
           </div>
         </div>
+      </div>
+
+      {/* pagination */}
+      <div className="text-center mb-3">
+        <h1 className="text-primary">&lt; 1...4 5 6...10 &gt;</h1>
       </div>
     </>
   )
