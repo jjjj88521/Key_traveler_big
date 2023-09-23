9a5edef1e409be8456e54c93cffd9ed0623e8b59
HsienLu <sjps946403@gmail.com>
2023-09-23 10:23:24 +0800

    完成login 頁面

--- next-client/pages/user/login.js
+++ next-client/pages/user/login.js
@@ -1,27 +1,51 @@
 import React from 'react'
-import Link from 'next/link'
 import Image from 'next/image'
 
 export default function Login() {
   return (
     <>
-      <div class="container">
-        <Image src="" alt="" />
-        <section>
-          <label htmlFor="account">帳號</label>
-          <input type="text" name="account" id="account" />
-        </section>
-        <section>
-          <label htmlFor="password">密碼</label>
-          <input type="text" name="password" id="password" />
-        </section>
-        <button class="btn btn-primary">登入</button>
-        <section>
-          <p>
-            還不是會員嗎?<a href="">立刻註冊</a>
+      <div className="container ">
+        <div className="logo mb-3 text-center ">
+          {' '}
+          <Image
+            src="/images/header-logo-desktop.png"
+            width={250}
+            height={46}
+            alt="logo"
+            className=""
+          />
+        </div>
+        <form className="w-50 mx-auto">
+          <div className="row mb-3 ">
+            <label htmlFor="account" className="form-label px-0">
+              帳號
+            </label>
+            <input
+              type="text"
+              name="account"
+              id="account"
+              className="form-control"
+            />
+          </div>
+          <div className="row mb-3">
+            <label htmlFor="password" className="form-label px-0">
+              密碼
+            </label>
+            <input
+              type="text"
+              name="password"
+              id="password"
+              className="form-control "
+            />
+          </div>
+          <button class="btn btn-primary my-3 w-100">登入</button>
+        </form>
+        <div className=" text-center my-3">
+          <p className=" ">
+            還不是會員嗎?<a href=""> 立刻註冊</a>
           </p>
-          <a>回上一頁</a>
-        </section>
+          <a className="  ">回上一頁</a>
+        </div>
       </div>
     </>
   )
