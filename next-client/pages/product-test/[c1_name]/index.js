c203349eb13463ae6c6c30e42b8fc9a83933dd29
jjjj88521 <jjjj88521@gmail.com>
2023-09-25 14:26:44 +0800

    product 測試

--- next-client/pages/product-test/[c1_name]/index.js
+++ next-client/pages/product-test/[c1_name]/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 export default function ProductCate1() {
+  const router = useRouter()
+  const { c1_name } = router.query
+  console.log(c1_name)
   return <div>ProductCate1</div>
 }
