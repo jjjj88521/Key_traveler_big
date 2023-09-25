3ec8715c8756fb192384ebfb988319f503ba30a6
ning <d825097@gmail.com>
2023-09-25 20:59:18 +0800

    0925 訂單明細頁及 購物車 step1

--- next-client/pages/cart/index.js
+++ next-client/pages/cart/index.js
@@ -1,14 +1,37 @@
-import { Input } from 'antd'
 import React from 'react'
+import { Steps } from 'antd'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faCircleChevronDown,
+  faTrashCan,
+  faCaretDown,
+} from '@fortawesome/free-solid-svg-icons'
 
 export default function Cart() {
+  const items = [
+    {
+      title: '確認商品',
+    },
+    {
+      title: '填寫訂單資訊',
+    },
+    {
+      title: '完成訂單',
+    },
+  ]
   return (
     <>
       <div className="container">
-        <h1 className="text-primary">購物車清單</h1>
+        <Steps current={0} labelPlacement="vertical" items={items} />
+        <h1 className="text-primary fs-3 pt-5 pb-3">購物車清單</h1>
         {/* 一般商品 */}
         <div className="mb-3 text-primary d-none d-sm-block d-sm-flex">
-          <div>V</div>
+          <div className="pe-2">
+            <FontAwesomeIcon
+              icon={faCircleChevronDown}
+              className="text-primary"
+            />
+          </div>
           <div>一般商品</div>
           <div className="ps-1">(2)</div>
         </div>
@@ -19,10 +42,17 @@ export default function Cart() {
               <th className="bg-primary text-white text-center align-middle">
                 <input type="checkbox" />
               </th>
-              <th className="bg-primary text-white ps-3">商品明細</th>
-              <th className="bg-primary text-white">單價</th>
-              <th className="bg-primary text-white">數量</th>
-              <th className="bg-primary text-white">小計</th>
+              <th
+                className="bg-primary text-white ps-3"
+                style={{ width: '40%' }}
+              >
+                商品明細
+              </th>
+              <th className="bg-primary text-white ">單價</th>
+              <th className="bg-primary text-white" style={{ width: '20%' }}>
+                數量
+              </th>
+              <th className="bg-primary text-white ">小計</th>
               <th className="bg-primary text-white"></th>
             </tr>
           </thead>
@@ -42,13 +72,17 @@ export default function Cart() {
                 <div className="p-2">
                   <div>Qwertykey</div>
                   <div>QK75鍵盤鍵盤鍵盤鍵盤</div>
-                  <div className="pb-1">
-                    <select className="form-select form-select-sm">
+                  <div className="pt-1">
+                    <select
+                      className="form-select form-select-sm mb-1"
+                      style={{ width: 140 }}
+                    >
                       <option>陽極紅</option>
                     </select>
-                  </div>
-                  <div>
-                    <select className="form-select form-select-sm">
+                    <select
+                      className="form-select form-select-sm"
+                      style={{ width: 140 }}
+                    >
                       <option>噴砂銀</option>
                     </select>
                   </div>
@@ -56,17 +90,27 @@ export default function Cart() {
               </td>
               <td className="align-middle">$3000</td>
               <td className="align-middle ps-3">
-                <div>
-                  <span>-</span>
-                  <input type="text" />
-                  <span>+</span>
+                <div className="input-group w-75">
+                  <span className="input-group-text p-0">
+                    <button className="btn" type="button">
+                      -
+                    </button>
+                  </span>
+                  <input type="number" className="form-control " />
+                  <span className="input-group-text p-0">
+                    <button className="btn" type="button">
+                      +
+                    </button>
+                  </span>
                 </div>
               </td>
               <td className="align-middle">$3000</td>
-              <td className="align-middle">刪除</td>
+              <td className="align-middle">
+                <FontAwesomeIcon icon={faTrashCan} className="text-primary" />
+              </td>
             </tr>
             <tr>
-              <td className="text-end" colSpan={6}>
+              <td className="pe-3 text-end" colSpan={6}>
                 總計: $6000
               </td>
             </tr>
@@ -74,7 +118,12 @@ export default function Cart() {
         </table>
         {/* 租用商品 */}
         <div className="mb-3 text-primary d-none d-sm-block d-sm-flex">
-          <div>V</div>
+          <div className="pe-2">
+            <FontAwesomeIcon
+              icon={faCircleChevronDown}
+              className="text-primary"
+            />
+          </div>
           <div>租用商品</div>
           <div className="ps-1">(2)</div>
         </div>
@@ -82,13 +131,23 @@ export default function Cart() {
         <table className={`table d-none d-sm-table`}>
           <thead>
             <tr>
-              <th className="bg-primary text-white text-center align-middle">
+              <th
+                className="bg-primary text-white text-center align-middle"
+                style={{ width: '5%' }}
+              >
                 <input type="checkbox" />
               </th>
-              <th className="bg-primary text-white ps-3">商品明細</th>
-              <th className="bg-primary text-white">租用日期</th>
-              <th className="bg-primary text-white">數量</th>
-              <th className="bg-primary text-white">小計</th>
+              <th
+                className="bg-primary text-white ps-3"
+                style={{ width: '40%' }}
+              >
+                商品明細
+              </th>
+              <th className="bg-primary text-white" style={{ width: '20%' }}>
+                租用日期
+              </th>
+              {/* <th className="bg-primary text-white">數量</th> */}
+              <th className="bg-primary text-white text-center">小計</th>
               <th className="bg-primary text-white"></th>
             </tr>
           </thead>
@@ -108,34 +167,59 @@ export default function Cart() {
                 <div className="p-2">
                   <div>Qwertykey</div>
                   <div>QK75鍵盤鍵盤鍵盤鍵盤</div>
-                  <div className="pb-1">
-                    <select className="form-select form-select-sm">
+                  <div className="pt-1">
+                    <select
+                      className="form-select form-select-sm mb-1"
+                      style={{ width: 140 }}
+                    >
                       <option>陽極紅</option>
                     </select>
-                  </div>
-                  <div>
-                    <select className="form-select form-select-sm">
+                    <select
+                      className="form-select form-select-sm"
+                      style={{ width: 140 }}
+                    >
                       <option>噴砂銀</option>
                     </select>
                   </div>
+                  {/* <div>
+                    <select className="form-select form-select-sm">
+                      <option>噴砂銀</option>
+                    </select>
+                  </div> */}
                 </div>
               </td>
               <td className="align-middle">
                 <input className="form-control" type="date" />
+                <div className="text-center">
+                  <FontAwesomeIcon
+                    icon={faCaretDown}
+                    className="text-secondary"
+                  />
+                </div>
                 <input className="form-control" type="date" />
               </td>
-              <td className="align-middle ps-3">
-                <div>
-                  <span>-</span>
-                  <input type="text" />
-                  <span>+</span>
+              {/* <td className="align-middle ps-3">
+                <div className="input-group">
+                  <span className="">
+                    <button type="button" className="btn btn-outline-secondary">
+                      -
+                    </button>
+                  </span>
+                  <input type="number" className="form-control" />
+                  <span className="">
+                    <button type="button" className="btn btn-outline-secondary">
+                      +
+                    </button>
+                  </span>
                 </div>
+              </td> */}
+              <td className="align-middle text-center">$3000</td>
+              <td className="align-middle">
+                <FontAwesomeIcon icon={faTrashCan} className="text-primary" />
               </td>
-              <td className="align-middle">$3000</td>
-              <td className="align-middle">刪除</td>
             </tr>
             <tr>
-              <td className="text-end" colSpan={6}>
+              <td className="pe-5 text-end" colSpan={6}>
                 總計: $6000
               </td>
             </tr>
@@ -153,7 +237,9 @@ export default function Cart() {
                 <div className="d-flex">
                   <div>一般商品</div>
                   <div className="ps-1">(2)</div>
-                  <div className="ms-auto">V</div>
+                  <div className="ms-auto">
+                    <FontAwesomeIcon icon={faCircleChevronDown} />
+                  </div>
                 </div>
               </th>
               {/* <th className="bg-primary text-white text-end" colSpan={2}>V</th> */}
@@ -175,21 +261,36 @@ export default function Cart() {
                 <div>
                   <div>Qwertykey</div>
                   <div>QK75鍵盤鍵盤鍵盤鍵盤</div>
-                  <div className="pb-1">
-                    <select className="form-select form-select-sm">
+                  <div className="pt-1">
+                    <select
+                      className="form-select form-select-sm  mb-1"
+                      style={{ width: '50%' }}
+                    >
                       <option>陽極紅</option>
-                    </select>
+                    </select>  
+                      <select
+                        className="form-select form-select-sm"
+                        style={{ width: '50%' }}
+                      >
+                        <option>噴砂銀</option>
+                      </select>
+                    
                   </div>
-                  <div>
-                    <select className="form-select form-select-sm">
-                      <option>噴砂銀</option>
-                    </select>
-                  </div>
-                  <div className="d-flex">
-                    <div>$3000</div>
-                    <button className="border border-secondary bg-light ms-auto">
-                      1
-                    </button>
+                  <div className="d-flex pt-1">
+                    <div className='pt-2'>$3000</div>
+                    <div className="input-group" style={{ width: '60%' }}>
+                      {/* <span className="input-group-text p-0"></span> */}
+                        <button className="btn btn-outline-secondary rounded-start-4" type="button">
+                          -
+                        </button>
+                      
+                      <input type="number" className="form-control border-secondary border-start-0 border-end-0" />
+                      <span className="input-group-text p-0"></span>
+                        <button className="btn btn-outline-secondary rounded-end-4" type="button">
+                          +
+                        </button>
+                      
+                    </div>
                   </div>
                 </div>
               </td>
@@ -223,7 +324,9 @@ export default function Cart() {
                 <div className="d-flex">
                   <div>租用商品</div>
                   <div className="ps-1">(2)</div>
-                  <div className="ms-auto">V</div>
+                  <div className="ms-auto">
+                    <FontAwesomeIcon icon={faCircleChevronDown} />
+                  </div>
                 </div>
               </th>
             </tr>
@@ -244,22 +347,42 @@ export default function Cart() {
                 <div>
                   <div>Qwertykey</div>
                   <div>QK75鍵盤鍵盤鍵盤鍵盤</div>
-                  <div className="pb-1">
-                    <select className="form-select form-select-sm">
+                  <div className="pt-1">
+                    <select
+                      className="form-select form-select-sm  mb-1"
+                      style={{ width: '50%' }}
+                    >
                       <option>陽極紅</option>
-                    </select>
-                  </div>
-                  <div>
-                    <select className="form-select form-select-sm">
-                      <option>噴砂銀</option>
-                    </select>
+                    </select>  
+                      <select
+                        className="form-select form-select-sm"
+                        style={{ width: '50%' }}
+                      >
+                        <option>噴砂銀</option>
+                      </select>                    
                   </div>
                   <div>8/15 ~ 8/16</div>
                   <div className="d-flex">
                     <div>$300</div>
-                    <button className="border border-secondary bg-light ms-auto">
-                      1
-                    </button>
+                    {/* <div className="input-group">
+                      <span>
+                        <button
+                          type="button"
+                          className="btn btn-outline-secondary"
+                        >
+                          -
+                        </button>
+                      </span>
+                      <input type="number" className="form-control" />
+                      <span>
+                        <button
+                          type="button"
+                          className="btn btn-outline-secondary"
+                        >
+                          +
+                        </button>
+                      </span>
+                    </div> */}
                   </div>
                 </div>
               </td>
@@ -271,6 +394,23 @@ export default function Cart() {
             </tr>
           </tbody>
         </table>
+        {/* 去結帳 */}
+        <div className="my-3">
+          <div>
+            使用優惠券(僅限一般商品):{' '}
+            <a className="text-primary text-decoration-none">更換優惠券</a>
+          </div>
+          <div className="text-danger">
+            P.S.若商品種類(一般、團購、租用)相同，產品數量多且收件地址不同需分開結帳！！
+          </div>
+          <div className="text-end">
+            <span className="align-middle">
+              共<span className="">6</span>件商品, 總金額: $
+              <span className="">1800</span>
+            </span>
+            <a className="btn btn-primary text-white ms-2">去結帳</a>
+          </div>
+        </div>
       </div>
     </>
   )
--- next-client/pages/user/order/[oid].js
+++ next-client/pages/user/order/[oid].js
@@ -1,4 +1,9 @@
 import React from 'react'
+import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { faPencil } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function Detail() {
   return (
@@ -6,7 +11,7 @@ export default function Detail() {
       <div className="container">
       {/* 一般商品 */}
         <div className="mb-3 text-primary d-none d-sm-block d-sm-flex">
-          <div>V</div>
+          <div><FontAwesomeIcon icon={faCircleChevronDown} className="text-primary" /></div>
           <div>一般商品</div>
           <div className="ps-1">(2)</div>
         </div>
@@ -25,7 +30,7 @@ export default function Detail() {
             <tr>
               <td className="d-flex">
                 <div className="p-2">
-                  <img
+                  <Image
                     src="/images/1669370674683000804.jpg"
                     width={100}
                     height={100}
@@ -41,12 +46,12 @@ export default function Detail() {
               <td className="align-middle">$3000</td>
               <td className="align-middle ps-3">1</td>
               <td className="align-middle">$3000</td>
-              <td className="align-middle">評價</td>
+              <td className="align-middle ps-3"><FontAwesomeIcon icon={faPencil} className="text-primary" /></td>
             </tr>
             <tr>
               <td className="d-flex">
                 <div className="p-2">
-                  <img
+                  <Image
                     src="/images/1669370674683000804.jpg"
                     width={100}
                     height={100}
@@ -62,7 +67,7 @@ export default function Detail() {
               <td className="align-middle">$3000</td>
               <td className="align-middle ps-3">1</td>
               <td className="align-middle">$3000</td>
-              <td className="align-middle">評價</td>
+              <td className="align-middle ps-3"><FontAwesomeIcon icon={faPencil} className="text-primary" /></td>
             </tr>
             <tr>
               <td className="text-end" colSpan={5}>
@@ -73,7 +78,7 @@ export default function Detail() {
         </table>
         {/* 租用商品 */}
         <div className="mb-3 text-primary d-none d-sm-block d-sm-flex">
-          <div>V</div>
+          <div><FontAwesomeIcon icon={faCircleChevronDown} className="text-primary" /></div>
           <div>租用商品</div>
           <div className="ps-1">(2)</div>
         </div>
@@ -91,7 +96,7 @@ export default function Detail() {
             <tr>
               <td className="d-flex">
                 <div className="p-2">
-                  <img
+                  <Image
                     src="/images/1669370674683000804.jpg"
                     width={100}
                     height={100}
@@ -148,7 +153,7 @@ export default function Detail() {
                 <div className="d-flex px-1">
                   <div>一般商品</div>
                   <div className="ps-1">(2)</div>
-                  <div className="ms-auto">V</div>
+                  <div className="ms-auto"><FontAwesomeIcon icon={faCircleChevronDown} /></div>
                 </div>
               </th>
               {/* <th className="bg-primary text-white text-end" colSpan={2}>V</th> */}
@@ -158,7 +163,7 @@ export default function Detail() {
             <tr>
               <td className="d-flex">
                 <div className="pe-2 pt-2">
-                  <img
+                  <Image
                     src="/images/000408000035028.jpg"
                     width={100}
                     height={100}
@@ -187,19 +192,19 @@ export default function Detail() {
                   <button>1</button>
                 </div>
               </td> */}
-              <td>評價</td>
+              <td className='align-middle'><FontAwesomeIcon icon={faPencil} className="text-primary" /></td>
             </tr>
             <tr>
               <td className="d-flex">
                 <div className="pe-2 pt-2">
-                  <img
+                  <Image
                     src="/images/000408000035028.jpg"
                     width={100}
                     height={100}
                   />
                 </div>
                 <div>
-                  <div>Qwertykey</div>
+                  <div className="">Qwertykey</div>
                   <div>QK75鍵盤鍵盤鍵盤鍵盤</div>
                   <div>陽極紅</div>
                   <div>噴砂銀</div>
@@ -211,7 +216,7 @@ export default function Detail() {
                   </div>
                 </div>
               </td>
-              <td>評價</td>
+              <td className='align-middle'><FontAwesomeIcon icon={faPencil} className="text-primary" /></td>
             </tr>
             <tr>
               <td className="text-end" colSpan={2}>
@@ -229,7 +234,7 @@ export default function Detail() {
                 <div className="d-flex px-1">
                   <div>租用商品</div>
                   <div className="ps-1">(2)</div>
-                  <div className="ms-auto">V</div>
+                  <div className="ms-auto"><FontAwesomeIcon icon={faCircleChevronDown} /></div>
                 </div>
               </th>
             </tr>
@@ -238,7 +243,7 @@ export default function Detail() {
             <tr>
               <td className="d-flex">
                 <div className="pe-2 pt-2">
-                  <img
+                  <Image
                     src="/images/000408000035028.jpg"
                     width={100}
                     height={100}
@@ -262,7 +267,7 @@ export default function Detail() {
             <tr>
               <td className="d-flex">
                 <div className="pe-2 pt-2 ">
-                  <img
+                  <Image
                     src="/images/000408000035028.jpg"
                     width={100}
                     height={100}
