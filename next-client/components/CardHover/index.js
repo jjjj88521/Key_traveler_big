b6ef65f332738bde99801caba4b39c6d6330d8f3
lai <trumpet.0417@gmail.com>
2023-09-27 02:09:15 +0800

    商品card effect hover完成

--- next-client/components/CardHover/index.js
+++ next-client/components/CardHover/index.js
@@ -10,28 +10,29 @@ export default function CardHover() {
           .map((_, index) => (
             <div className="col " key={index}>
               <div className="col">
-                {/* <div className={`card ${styles['card']}`}> */}
-                <div className="card">
+                <div className={`card ${styles['card']}`}>
+                  {/* <div className="card"> */}
                   <img
                     src="/images/card.png"
                     className="card-img-top"
                     alt="..."
                   />
-                  <div className={`card-body ${styles['card']}`}>
-                    {/* <div className="card-body"> */}
+                  {/* <div className={`card-body ${styles['card']}`}> */}
+                  <div className="card-body">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">
                       Some quick example text to build on the card title and
                       make up the bulk of the cards content.
                     </p>
-                    <div class={styles['info']}>
-                      <h1>Mountain</h1>
-                      <p>
-                        Lorem Ipsum is simply dummy text from the printing and
-                        typesetting industry
-                      </p>
-                      <button>Read More</button>
-                    </div>
+                  </div>
+                  <div className={`d-flex flex-column gap-3 ${styles['info']}`}>
+                    <button className={styles['infoBtn1']}>加入購物車</button>
+                    <button className={styles['infoBtn2']}>
+                      <i class="fa-regular fa-heart"></i> Like
+                    </button>
+                    <button className={styles['infoBtn3']}>
+                      <i class="fa-solid fa-angles-right"></i> Learn More
+                    </button>
                   </div>
                 </div>
               </div>
