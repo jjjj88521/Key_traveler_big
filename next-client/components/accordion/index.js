c4e6ce18b638e8cea6c17421f6a1721b30edc739
Larry <trumpet.0417@gmail.com>
2023-09-26 09:57:36 +0800

    更新accordion預設全部關閉

--- next-client/components/accordion/index.js
+++ next-client/components/accordion/index.js
@@ -7,11 +7,11 @@ function Accordion() {
       <div className="accordion-item">
         <h2 className="accordion-header">
           <button
-            className="accordion-button bg-primary text-center text-light fw-bold fs-5 d-block text-center"
+            className="accordion-button collapsed bg-primary text-center text-light fw-bold fs-5 d-block text-center"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#panelsStayOpen-collapseOne"
-            aria-expanded="true"
+            aria-expanded="false"
             aria-controls="panelsStayOpen-collapseOne"
           >
             套件
@@ -19,7 +19,7 @@ function Accordion() {
         </h2>
         <div
           id="panelsStayOpen-collapseOne"
-          className="accordion-collapse collapse show"
+          className="accordion-collapse collapse"
         >
           <div className={`accordion-body ${styles['accBody']}`}>
             <ul>
@@ -37,44 +37,51 @@ function Accordion() {
       <div className="accordion-item">
         <h2 className="accordion-header">
           <button
-            className="accordion-button collapsed"
+            className="accordion-button collapsed bg-primary text-center text-light fw-bold fs-5 d-block text-center"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#panelsStayOpen-collapseTwo"
             aria-expanded="false"
             aria-controls="panelsStayOpen-collapseTwo"
           >
-            Accordion Item #2
+            軸體
           </button>
         </h2>
         <div
           id="panelsStayOpen-collapseTwo"
           className="accordion-collapse collapse"
         >
-          <div className="accordion-body">
-            This is the third accordion body.
+          <div className={`accordion-body ${styles['accBody']}`}>
+            <ul>
+              <li>段落軸</li>
+              <li>線性軸</li>
+            </ul>
           </div>
         </div>
       </div>
       <div className="accordion-item">
         <h2 className="accordion-header">
           <button
-            className="accordion-button collapsed"
+            className="accordion-button collapsed bg-primary text-center text-light fw-bold fs-5 d-block text-center"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#panelsStayOpen-collapseThree"
             aria-expanded="false"
             aria-controls="panelsStayOpen-collapseThree"
           >
-            Accordion Item #3
+            鍵帽
           </button>
         </h2>
         <div
           id="panelsStayOpen-collapseThree"
           className="accordion-collapse collapse"
         >
-          <div className="accordion-body">
-            This is the third accordion body.
+          <div className={`accordion-body ${styles['accBody']}`}>
+            <ul>
+              <li>Cerry原廠高</li>
+              <li>OEM高</li>
+              <li>個性鍵帽</li>
+            </ul>
           </div>
         </div>
       </div>
