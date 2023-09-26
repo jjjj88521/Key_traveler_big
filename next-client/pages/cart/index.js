a819aed6d45bfe6164f5bd71dccaa9988c993635
ning <d825097@gmail.com>
2023-09-26 18:08:03 +0800

    0926 cart

--- next-client/pages/cart/index.js
+++ next-client/pages/cart/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { useState } from 'react'
-import { Steps, DatePicker } from 'antd'
+import React, { useState } from 'react'
+import style from './cart.module.scss'
+import { Steps, DatePicker, Divider, Radio, List } from 'antd'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faCircleChevronDown,
@@ -9,6 +9,7 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 
 export default function Cart() {
+  // step 1
   const { RangePicker } = DatePicker
   const items = [
     {
@@ -21,10 +22,113 @@ export default function Cart() {
       title: '完成訂單',
     },
   ]
+  // step 2
+  const cardListData = [
+    {
+      value: '1',
+      bank: 'xx銀行',
+      last4num: '1234',
+    },
+    {
+      value: '2',
+      bank: 'yy銀行',
+      last4num: '5678',
+    },
+    {
+      value: '3',
+      bank: 'zz銀行',
+      last4num: '9012',
+    },
+  ]
+  // 收件人資料_S
+  const [buyerValue, setbuyerValue] = useState(1)
+  const buyeronChange = (e) => {
+    console.log('buyer checked', e.target.value)
+    setbuyerValue(e.target.value)
+    if (e.target.value === 1) {
+      document.querySelector('.buyerSame').classList.remove('d-none')
+      document.querySelector('.buyerSame').classList.add('d-block')
+      document.querySelector('.buyerDiff').classList.remove('d-block')
+      document.querySelector('.buyerDiff').classList.add('d-none')
+    } else if (e.target.value === 2) {
+      document.querySelector('.buyerDiff').classList.remove('d-none')
+      document.querySelector('.buyerDiff').classList.add('d-block')
+      document.querySelector('.buyerSame').classList.remove('d-block')
+      document.querySelector('.buyerSame').classList.add('d-none')
+    } else {
+      alert('error')
+      return false
+    }
+  }
+  // 收件人資料_E
+
+  // 宅配方式_S
+  const [recProductValue, setRecProductValue] = useState(1)
+  const recProductonChange = (e) => {
+    console.log('receive product checked', e.target.value)
+    setRecProductValue(e.target.value)
+  }
+  // 宅配方式_E
+
+  // 付款方式_S
+  const [paymentValue, setPaymentValue] = useState(1)
+  const paymentonChange = (e) => {
+    console.log('payment checked', e.target.value)
+    setPaymentValue(e.target.value)
+    if (e.target.value === 1) {
+      document.querySelector('.payForOnline').classList.remove('d-none')
+      document.querySelector('.payForOnline').classList.add('d-block')
+    } else if (e.target.value === 2) {
+      document.querySelector('.payForOnline').classList.remove('d-block')
+      document.querySelector('.payForOnline').classList.add('d-none')
+    } else {
+      alert('error')
+      return false
+    }
+  }
+  // 付款方式_E
+
+  // 信用卡選取_S
+  const [selectedPayment, setSelectedPayment] = useState(1)
+  const handleRadioChange = (e) => {
+    console.log('payment card checked', e.target.value)
+
+    setSelectedPayment(e.target.value)
+  }
+  // 信用卡選取_E
+
+  // 信用卡新增_S
+  const [paymentData, setPaymentData] = useState(cardListData)
+  const [newBank, setNewBank] = useState('aa銀行')
+  const [newLast4num, setNewLast4num] = useState('')
+
+  const addPaymentOption = () => {
+    console.log('newBank is:' + newBank + ',newLast4num is:' + newLast4num)
+    if (newBank.trim() !== '' && newLast4num.trim() !== '') {
+      // 创建一个新选项并添加到数据源中
+      const newItem = {
+        value: String(paymentData.length + 1),
+        bank: newBank,
+        last4num: newLast4num,
+      }
+      setPaymentData([...paymentData, newItem])
+
+      // 清空输入框
+      setNewBank('aa銀行')
+      setNewLast4num('')
+    }
+  }
+
   return (
     <>
+      {/* step 1 */}
       <div className="container">
-        <Steps current={0} labelPlacement="vertical" items={items} />
+        <Steps
+          current={0}
+          labelPlacement="vertical"
+          items={items}
+          responsive="false"
+        />
         <h1 className="text-primary fs-3 pt-5 pb-3">購物車清單</h1>
         {/* 一般商品 */}
         <div className="mb-3 text-primary d-none d-sm-block d-sm-flex">
@@ -356,11 +460,11 @@ export default function Cart() {
         </table>
         {/* 去結帳 */}
         <div className="my-3">
-          <div className="pb-2">
-            <span className="">
-              使用優惠券(僅限一般商品): <span>全站9折</span>
+          <div className="pb-2 d-flex">
+            <span className="fs-6">
+              使用優惠券(限一般商品): <span>全站85折</span>
             </span>
-            <div class="btn-group ms-3">
+            <div class="btn-group ms-3 ">
               <button
                 class="btn btn-sm border-primary text-primary dropdown-toggle"
                 type="button"
@@ -395,6 +499,346 @@ export default function Cart() {
           </div>
         </div>
       </div>
+      {/* step 2 */}
+      {/* 信用卡新增_E */}
+      <div className="container mt-5">
+        <div className="d-flex justify-content-center mb-3">
+          <Steps
+            current={1}
+            labelPlacement="vertical"
+            items={items}
+            className="d-flex align-self-center"
+            style={{ width: '70%' }}
+          />
+        </div>
+        <div className="orderInfo">
+          <div className={`${style['buyerInfo']}`}>
+            <div className={`${style['cart_subtitle']}`}>訂購人資訊</div>
+            <div
+              className={`d-flex align-items-center justify-content-between ${style['info']}`}
+            >
+              <div className={`col-3 border ${style['infoLeft']}`}>姓名</div>
+              <div className={`col-9 border ${style['infoRight']}`}>
+                會員姓名
+              </div>
+            </div>
+            <div
+              className={`d-flex align-items-center justify-content-between ${style['info']}`}
+            >
+              <div className={`col-3 border ${style['infoLeft']}`}>Email</div>
+              <div className={`col-9 border ${style['infoRight']}`}>
+                會員Email
+              </div>
+            </div>
+            <div
+              className={`d-flex align-items-center justify-content-between ${style['info']}`}
+            >
+              <div className={`col-3 border ${style['infoLeft']}`}>電話</div>
+              <div className={`col-9 border ${style['infoRight']}`}>
+                會員電話
+              </div>
+            </div>
+            <div
+              className={`d-flex align-items-center justify-content-between ${style['info']}`}
+            >
+              <div className={`col-3 border ${style['infoLeft']}`}>地址</div>
+              <div className={`col-9 border ${style['infoRight']}`}>
+                會員地址
+              </div>
+            </div>
+          </div>
+          <div className="buyerInfoMobile mt-4 d-none"></div>
+          <div className={`${style['getProductInfo']}`}>
+            <div className={`${style['cart_subtitle']}`}>取貨方式</div>
+            <div className="radioNotUse">
+              {/* <div className={`${style['info']}`}>
+            <label>
+              <input
+                type="radio"
+                name="radioGroup"
+                value="option1"
+                checked
+                style={{ marginRight: '5px', marginLeft: '30px' }}
+              />
+              宅配到府
+            </label>
+          </div> */}
+            </div>
+            <Radio.Group
+              onChange={recProductonChange}
+              value={recProductValue}
+              className={`${style['info']}`}
+              style={{ marginRight: '5px', marginLeft: '30px' }}
+            >
+              <Radio value={1}>宅配到府</Radio>
+            </Radio.Group>
+          </div>
+          <div className={`${style['receiverInfo']}`}>
+            <div
+              className={`${style['cart_subtitle']} d-flex align-items-center`}
+            >
+              收件人資訊
+              <Radio.Group
+                onChange={buyeronChange}
+                value={buyerValue}
+                className="ms-3"
+              >
+                <Radio value={1} className="text-light">
+                  同訂購人
+                </Radio>
+                <Radio value={2} className="text-light">
+                  修改收件人資料
+                </Radio>
+              </Radio.Group>
+            </div>
+            <div>
+              <div className="buyerSame">
+                <div
+                  className={`d-flex align-items-center justify-content-between ${style['info']}`}
+                >
+                  <div className={`col-3 border ${style['infoLeft']}`}>
+                    姓名
+                  </div>
+                  <div className={`col-9 border ${style['infoRight']}`}>
+                    會員姓名
+                  </div>
+                </div>
+                <div
+                  className={`d-flex align-items-center justify-content-between ${style['info']}`}
+                >
+                  <div className={`col-3 border ${style['infoLeft']}`}>
+                    電話
+                  </div>
+                  <div className={`col-9 border ${style['infoRight']}`}>
+                    會員電話
+                  </div>
+                </div>
+                <div
+                  className={`d-flex align-items-center justify-content-between ${style['info']}`}
+                >
+                  <div className={`col-3 border ${style['infoLeft']}`}>
+                    地址
+                  </div>
+                  <div className={`col-9 border ${style['infoRight']}`}>
+                    會員地址
+                  </div>
+                </div>
+              </div>
+              <div className="buyerDiff d-none">
+                <div
+                  className={`d-flex align-items-center justify-content-between ${style['info']}`}
+                >
+                  <div className={`col-3 border ${style['infoLeft']}`}>
+                    姓名
+                  </div>
+                  <div
+                    className={`col-9 border ${style['infoRight']} h-100 d-flex align-items-center`}
+                  >
+                    <input
+                      type="text"
+                      placeholder="memberName"
+                      className={`form-control ${style['inputMargin']}`}
+                    ></input>
+                  </div>
+                </div>
+                <div
+                  className={`d-flex align-items-center justify-content-between ${style['info']}`}
+                >
+                  <div className={`col-3 border ${style['infoLeft']}`}>
+                    電話
+                  </div>
+                  <div
+                    className={`col-9 border ${style['infoRight']} h-100 d-flex align-items-center`}
+                  >
+                    <input
+                      type="text"
+                      placeholder="memberTel"
+                      className={`form-control ${style['inputMargin']}`}
+                    ></input>
+                  </div>
+                </div>
+                <div
+                  className={`d-flex align-items-center justify-content-between ${style['info']}`}
+                >
+                  <div className={`col-3 border ${style['infoLeft']}`}>
+                    地址
+                  </div>
+                  <div
+                    className={`col-9 border ${style['infoRight']} h-100 d-flex align-items-center`}
+                  >
+                    <input
+                      type="text"
+                      placeholder="memberAddr"
+                      className={`form-control ${style['inputMargin']}`}
+                    ></input>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={`${style['paymentInfo']}`}>
+            <div className={`${style['cart_subtitle']}`}>付款方式</div>
+            <Radio.Group
+              onChange={paymentonChange}
+              value={paymentValue}
+              className={`${style['info']}`}
+              style={{ marginRight: '5px', marginLeft: '30px' }}
+            >
+              <Radio value={1}>信用卡付款</Radio>
+              <Radio value={2}>貨到付款</Radio>
+            </Radio.Group>
+            <div className="payForOnline">
+              <Divider style={{ marginTop: '5px' }} />
+              <Radio.Group onChange={handleRadioChange} value={selectedPayment}>
+                <List
+                  style={{ marginTop: '-10px', marginBottom: '15px' }}
+                  size="small"
+                  split={false}
+                  dataSource={paymentData}
+                  renderItem={(item) => (
+                    <List.Item>
+                      <Radio
+                        value={item.value}
+                        style={{ marginLeft: '30px', marginBottom: '5px' }}
+                      >
+                        <div className="d-flex align-items-center">
+                          <div>{item.bank}</div>
+                          <div className="ms-5">****{item.last4num}</div>
+                        </div>
+                      </Radio>
+                    </List.Item>
+                  )}
+                />
+              </Radio.Group>
+              {/* NewCard Modal_S */}
+              <div
+                className={`${style['payCard']} position-relative`}
+                id="modal"
+              >
+                <div
+                  class={`${style['dia']} d-none ps-4 pe-4`}
+                  id="dialog"
+                  style={{ paddingTop: '35px' }}
+                >
+                  <div
+                    className={`${style['cardNum']} ${style['newCardInfo']} d-flex align-items-center`}
+                  >
+                    <span style={{ width: '100px' }}>信用卡號</span>
+                    <div className="d-flex align-items-center">
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="newCardFst"
+                        maxLength={4}
+                      ></input>
+                      <p className="mb-0">-</p>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="newCardSec"
+                        maxLength={4}
+                      ></input>
+                      <p className="mb-0">-</p>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="newCardThr"
+                        maxLength={4}
+                      ></input>
+                      <p className="mb-0">-</p>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="newCardLst"
+                        maxLength={4}
+                        value={newLast4num}
+                        onChange={(e) => setNewLast4num(e.target.value)}
+                      ></input>
+                    </div>
+                  </div>
+                  <div
+                    className={`${style['newCardInfo']} d-flex justify-content-between align-items-center`}
+                  >
+                    <div
+                      className={`${style['cardDate']} col-7 d-flex justify-content-start align-items-center `}
+                    >
+                      <span>到期日</span>
+                      <div className="d-flex align-items-center">
+                        <input type="text" className="form-control"></input>
+                        <p className="mb-0">/</p>
+                        <input type="text" className="form-control"></input>
+                      </div>
+                    </div>
+                    <div
+                      className={`${style['cardCode']} col-auto d-flex justify-content-between align-items-center `}
+                      style={{ marginRight: '23px' }}
+                    >
+                      <span style={{ marginLeft: '-5px', marginRight: '20px' }}>
+                        驗證碼
+                      </span>
+                      <input type="text" className="form-control"></input>
+                    </div>
+                  </div>
+                  <div
+                    className={`${style['cardOwner']} ${style['newCardInfo']} d-flex align-items-center`}
+                  >
+                    <span>持卡人姓名</span>
+                    <input type="text" className="form-control"></input>
+                  </div>
+                  <input
+                    type="hidden"
+                    value={newBank}
+                    id="newBank"
+                    onChange={(e) => setNewBank(e.target.value)}
+                  ></input>
+                  <button
+                    class={`btn ${style['button_main']}`}
+                    id="newbtn"
+                    onClick={addPaymentOption}
+                  >
+                    新增
+                  </button>
+                  <button
+                    class={`btn ${style['button_reverse']}`}
+                    id="closebtn"
+                    onClick={() => {
+                      document.querySelector('#dialog').classList.add('d-none')
+                    }}
+                  >
+                    取消
+                  </button>
+                </div>
+                <button
+                  class={`btn ${style['button_main']} ms-4`}
+                  id="show"
+                  onClick={() => {
+                    document
+                      .querySelector('#dialog')
+                      .classList.add(`${style['dia']}`)
+                    document.querySelector('#dialog').classList.remove('d-none')
+                    document.querySelector('#dialog').classList.add('d-block')
+                  }}
+                >
+                  新增信用卡
+                </button>
+              </div>
+              {/* NewCard Modal_E */}
+            </div>
+          </div>
+        </div>
+        <div className="stepBtn d-flex justify-content-end">
+          <button
+            class={`btn ${style['button_reverse']} me-2`}
+            id="checkProduct"
+          >
+            上一步
+          </button>
+          <button class={`btn ${style['button_main']}`} id="completeOrder">
+            送出訂單
+          </button>
+        </div>
+      </div>
+      <Divider />
     </>
   )
 }
--- next-client/pages/user/order/index.js
+++ next-client/pages/user/order/index.js
@@ -1,72 +1,96 @@
 import React from 'react'
 // import style from '@/styles/order.module.scss'
+import UserSideBar from '../user-side-bar'
+import UserSideBarMobile from '../user-side-bar-mobile'
 
 export default function Order() {
   return (
     <>
       <div className="container">
-        {/* 歷史訂單列表頁 電腦版 */}
-        {/* {`table d-none d-sm-table`} */}
-        {/* {`${style['table-desktop']} table`} */}
-        <table className={`table d-none d-sm-table`}>
-          <thead className="">
-            <tr className="">
-              <th className="bg-primary"></th>
-              <th className="bg-primary text-white ps-5">訂單編號</th>
-              <th className="bg-primary text-white ps-5">訂單日期</th>
-              <th className="bg-primary text-white ps-5">訂單金額</th>
-              <th className="bg-primary text-white"></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="">
-              <td className="text-center">1</td>
-              <td className="ps-5">P00001</td>
-              <td className="ps-5">2023/08/15</td>
-              <td className="ps-5">1800</td>
-              <td className="ps-5">
-                <button className="btn btn-primary text-light">查看</button>
-              </td>
-            </tr>
-            <tr className="">
-              <td className="text-center">2</td>
-              <td className="ps-5">G00002</td>
-              <td className="ps-5">2023/08/16</td>
-              <td className="ps-5">800</td>
-              <td className="ps-5">
-                <button className="btn btn-primary text-light">查看</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        {/* 歷史訂單列表頁 手機版 */}
-        {/* {`table table-bordered d-table d-sm-none`} */}
-        {/* {`${style['table-mobile']} table table-bordered`} */}
-        <table className={`table table-bordered d-table d-sm-none`}>
-          <tbody>
-            <tr>
-              <th className="ps-3">1</th>
-              <td></td>
-            </tr>
-            <tr>
-              <th>訂單編號</th>
-              <td>P00001</td>
-            </tr>
-            <tr>
-              <th>訂單日期</th>
-              <td>2023/08/15</td>
-            </tr>
-            <tr>
-              <th>訂單金額</th>
-              <td>1800</td>
-            </tr>
-            <tr>
-              <td className="text-center" colSpan={2}>
-                <button className="btn btn-primary text-light">查看</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="row mb-sm-4 mb-2">
+          {' '}
+          <h2 className="fw-bolder text-start col-sm-3 col-12 mb-sm-0 mb-3">
+            歷史訂單
+          </h2>
+          <nav className=" d-flex justify-content-evenly col-sm-8  offset-sm-1  d-none d-sm-flex"></nav>
+        </div>
+        <div className="row">
+          <div className="col-sm-3 col-12 px-0 mx-0">
+            <div className="d-sm-block d-none">
+              <UserSideBar />
+            </div>
+            <div className="d-sm-none d-block col-12 mb-3">
+              <UserSideBarMobile className="col-12 w-100" />
+            </div>
+          </div>
+          <nav className=" d-flex justify-content-evenly col-sm-8  offset-sm-1 col-12 d-block d-sm-none  mt-4 mb-3">
+            
+          </nav>
+          <div className="col-sm-8 offset-sm-1 col-12">
+            {/* 歷史訂單列表頁 電腦版 */}
+            {/* {`table d-none d-sm-table`} */}
+            {/* {`${style['table-desktop']} table`} */}
+            <table className={`table d-none d-sm-table`}>
+              <thead className="">
+                <tr className="">
+                  <th className="bg-primary"></th>
+                  <th className="bg-primary text-white ps-5">訂單編號</th>
+                  <th className="bg-primary text-white ps-5">訂單日期</th>
+                  <th className="bg-primary text-white ps-5">訂單金額</th>
+                  <th className="bg-primary text-white"></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="">
+                  <td className="text-center">1</td>
+                  <td className="ps-5">P00001</td>
+                  <td className="ps-5">2023/08/15</td>
+                  <td className="ps-5">1800</td>
+                  <td className="ps-5">
+                    <button className="btn btn-primary text-light">查看</button>
+                  </td>
+                </tr>
+                <tr className="">
+                  <td className="text-center">2</td>
+                  <td className="ps-5">G00002</td>
+                  <td className="ps-5">2023/08/16</td>
+                  <td className="ps-5">800</td>
+                  <td className="ps-5">
+                    <button className="btn btn-primary text-light">查看</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            {/* 歷史訂單列表頁 手機版 */}
+            {/* {`table table-bordered d-table d-sm-none`} */}
+            {/* {`${style['table-mobile']} table table-bordered`} */}
+            <table className={`table table-bordered d-table d-sm-none`}>
+              <tbody>
+                <tr>
+                  <th className="ps-3">1</th>
+                  <td></td>
+                </tr>
+                <tr>
+                  <th>訂單編號</th>
+                  <td>P00001</td>
+                </tr>
+                <tr>
+                  <th>訂單日期</th>
+                  <td>2023/08/15</td>
+                </tr>
+                <tr>
+                  <th>訂單金額</th>
+                  <td>1800</td>
+                </tr>
+                <tr>
+                  <td className="text-center" colSpan={2}>
+                    <button className="btn btn-primary text-light">查看</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </>
   )
