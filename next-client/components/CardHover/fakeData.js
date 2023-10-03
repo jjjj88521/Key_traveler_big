73895940786c0fe63fdade1f94615b37695cadb8
Larry <trumpet.0417@gmail.com>
2023-10-03 11:42:56 +0800

    實現商品card塞假資料

---
+++ next-client/components/CardHover/fakeData.js
@@ -0,0 +1,44 @@
+const fakeData = [
+  {
+    title: '鍵盤 1',
+    description:
+      '確認好了回應引擎一顆小姐直接官方轉載時期人家不起，但他輸入差不多觀點缺乏中心需要蔬菜基隆總是部落格一道，分享排行報名能力將軍教育，好好微軟經濟品質分類詳細訊息僅供一塊股份應當，上漲也可可能會戰略練習主流高中究竟，此刻誠信我是不可以，身份究竟將來，裡面程式。',
+    price: 19.99,
+    imagePath:
+      'https://images.unsplash.com/photo-1695832224148-551b29209d2c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2100&q=80',
+  },
+  {
+    title: '鍵盤 2',
+    description:
+      '確認好了回應引擎一顆小姐直接官方轉載時期人家不起，但他輸入差不多觀點缺乏中心需要蔬菜基隆總是部落格一道，分享排行報名能力將軍教育，好好微軟經濟品質分類詳細訊息僅供一塊股份應當，上漲也可可能會戰略練習主流高中究竟，此刻誠信我是不可以，身份究竟將來，裡面程式。',
+    price: 29.99,
+    imagePath:
+      'https://images.unsplash.com/photo-1695792877550-8d3b4c1bfcd7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80',
+  },
+  {
+    title: '鍵盤 3',
+    description:
+      '確認好了回應引擎一顆小姐直接官方轉載時期人家不起，但他輸入差不多觀點缺乏中心需要蔬菜基隆總是部落格一道，分享排行報名能力將軍教育，好好微軟經濟品質分類詳細訊息僅供一塊股份應當，上漲也可可能會戰略練習主流高中究竟，此刻誠信我是不可以，身份究竟將來，裡面程式。',
+    price: 39.99,
+    imagePath:
+      'https://plus.unsplash.com/premium_photo-1691938887449-8f5ac5251a0d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80',
+  },
+  {
+    title: '鍵盤 4',
+    description:
+      '確認好了回應引擎一顆小姐直接官方轉載時期人家不起，但他輸入差不多觀點缺乏中心需要蔬菜基隆總是部落格一道，分享排行報名能力將軍教育，好好微軟經濟品質分類詳細訊息僅供一塊股份應當，上漲也可可能會戰略練習主流高中究竟，此刻誠信我是不可以，身份究竟將來，裡面程式。',
+    price: 49.99,
+    imagePath:
+      'https://plus.unsplash.com/premium_photo-1676213441458-4a87a6636791?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2068&q=80',
+  },
+  {
+    title: '鍵盤 5',
+    description:
+      '確認好了回應引擎一顆小姐直接官方轉載時期人家不起，但他輸入差不多觀點缺乏中心需要蔬菜基隆總是部落格一道，分享排行報名能力將軍教育，好好微軟經濟品質分類詳細訊息僅供一塊股份應當，上漲也可可能會戰略練習主流高中究竟，此刻誠信我是不可以，身份究竟將來，裡面程式。',
+    price: 59.99,
+    imagePath:
+      'https://plus.unsplash.com/premium_photo-1672829032578-67aae2e62363?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1986&q=80',
+  },
+]
+
+export default fakeData
--- next-client/components/CardHover/index.js
+++ next-client/components/CardHover/index.js
@@ -1,20 +1,24 @@
 // CardHover.js
 import React from 'react'
-import Card from './Card' // 引入新的Card组件
+import Card from './Card'
+import fakeData from './fakeData'
 
 export default function CardHover() {
   return (
     <>
-      <div className="d-flex justify-content-end row row-cols-2 row-cols-md-3 g-4 mb-sm-0 mb-4">
-        {Array(12)
-          .fill()
-          .map((_, index) => (
-            <div className="col" key={index}>
-              <div className="col">
-                <Card />
-              </div>
+      <div className="d-flex row row-cols-2 row-cols-md-3 g-4 mb-sm-0 mb-4">
+        {fakeData.map((product, index) => (
+          <div className="col" key={index}>
+            <div className="col">
+              <Card
+                title={product.title}
+                description={product.description}
+                price={product.price}
+                imagePath={product.imagePath}
+              />
             </div>
-          ))}
+          </div>
+        ))}
       </div>
     </>
   )
