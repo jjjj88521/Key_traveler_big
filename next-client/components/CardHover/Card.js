0b3f7bf99198a31860d26c3bbd9577e79e2476c7
Larry <trumpet.0417@gmail.com>
2023-10-03 11:42:16 +0800

    實現商品card塞假資料

--- next-client/components/CardHover/Card.js
+++ next-client/components/CardHover/Card.js
@@ -1,18 +1,27 @@
-// Card.js
 import React from 'react'
-import styles from './index.module.css'
+import styles from './card.module.css'
 
-export default function Card() {
+export default function Card({ title, description, price, imagePath }) {
+  // 截斷過長的商品介紹
+  function truncateText(text, maxLength) {
+    if (text.length <= maxLength) {
+      return text
+    } else {
+      return text.substring(0, maxLength) + '...'
+    }
+  }
+
   return (
-    <div className={`card ${styles['card']}`}>
-      <img src="/images/card.png" className="card-img-top" alt="..." />
+    <div className={`card h-auto ${styles['card']}`}>
+      <img
+        src={imagePath}
+        className={`card-img-top h-50 ${styles.cardImg}`}
+        alt="Product"
+      />
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the cards content.
-        </p>
-        <h5 className="card-title">$9999</h5>
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{truncateText(description, 50)}</p>
+        <h5 className="card-title">$ {price}</h5>
       </div>
       <div className={`d-flex flex-column gap-3 ${styles['info']}`}>
         <button className={styles['infoBtn1']}>加入購物車</button>
