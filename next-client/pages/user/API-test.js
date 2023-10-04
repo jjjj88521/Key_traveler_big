70a928c8a5bdee2658735fa0989a5ade14b07d1b
HsienLu <sjps946403@gmail.com>
2023-10-04 18:43:50 +0800

    20231004-API-test-串到資料庫了;下一步找localStorage

--- next-client/pages/user/API-test.js
+++ next-client/pages/user/API-test.js
@@ -1,19 +1,71 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
-
+import axios from 'axios'
+import { resolve } from 'styled-jsx/css'
 export default function ApiTest() {
   const [data, setData] = useState('')
+  // //有非同步、沒state=>不會無窮迴圈
+  // new Promise((resolve, reject) => {
+  //   axios
+  //     .get('http://localhost:3005/api/users')
+  //     .then((res) => {
+  //       console.log(res)
+  //       resolve()
+  //       // setData(res)
+  //     })
+  //     .catch((err) => {
+  //       console.log(err)
+  //     })
+  // })
+  // //有非同步、有state=>會無窮迴圈
+  // new Promise((resolve, reject) => {
+  //   axios
+  //     .get('http://localhost:3005/api/users')
+  //     .then((res) => {
+  //       console.log(res)
+  //       resolve()
+  //       setData(res)
+  //     })
+  //     .catch((err) => {
+  //       console.log(err)
+  //     })
+  // })
+
+  // //沒寫非同步、state=>不會無窮迴圈
+  // axios
+  //   .get('http://localhost:3005/api/users')
+  //   .then((res) => {
+  //     console.log(res)
+  //     // setData(res)
+  //   })
+  //   .catch((err) => {
+  //     console.log(err)
+  //   })
+  //沒寫非同步、有寫state=>會無窮迴圈
+  // axios
+  //   .get('http://localhost:3005/api/users')
+  //   .then((res) => {
+  //     console.log(res)
+  //     setData(res)
+  //   })
+  //   .catch((err) => {
+  //     console.log(err)
+  //   })
+  //沒寫非同步、有寫state、包effect=>不會無窮迴圈:解法
   useEffect(() => {
-    fetch('http://localhost:3005/api/users')
-      .then((response) => response.json())
-      .then((value) => setData(value))
-      .catch((error) => console.error('Error fetching data:', error))
+    axios
+      .get('http://localhost:3005/api/users')
+      .then((res) => {
+        console.log(res)
+        setData(res)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }, [])
   return (
     <>
       <h1>Test</h1>
-
-      {console.log(data)}
     </>
   )
 }
