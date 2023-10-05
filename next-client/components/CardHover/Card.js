9ff49b938c639f3f244f8815bc30206a44536009
Larry <trumpet.0417@gmail.com>
2023-10-05 10:17:02 +0800

    加入購物車button rwd完成

--- next-client/components/CardHover/Card.js
+++ next-client/components/CardHover/Card.js
@@ -18,10 +18,15 @@ export default function Card({ title, brand, price, imagePath }) {
         className={`card-img-top ${styles.cardImg}`}
         alt="Product"
       />
-      <div className="card-body w-100">
+      <div className="card-body w-100 position-relative">
         <h4 className="card-title">{title}</h4>
         <h6 className="card-title text-black-50">{brand}</h6>
         <h5 className="card-title">$ {price}</h5>
+        <button
+          className={`d-sm-none d-block bg-white border border-2 border-primary rounded-circle ${styles['cartBtn']}`}
+        >
+          <i className="fa-solid fa-cart-shopping fa-xl text-secondary"></i>
+        </button>
       </div>
       <div className={`d-flex flex-column gap-3 ${styles['info']}`}>
         <button className={styles['infoBtn1']}>加入購物車</button>
