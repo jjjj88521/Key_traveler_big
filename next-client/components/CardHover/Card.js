93caa6415cd97ab70978651648c26b257ee700f8
Larry <trumpet.0417@gmail.com>
2023-10-05 15:47:01 +0800

    修改card相關component前備份

--- next-client/components/CardHover/Card.js
+++ next-client/components/CardHover/Card.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './card.module.scss'
+import { useRouter } from 'next/router'
+import ProductFetcher from './ProductFetcher'
 
 export default function Card({ title, brand, price, imagePath }) {
+  const [data, setData] = useState(null)
+  console.log(data)
+  const router = useRouter()
+
+  const onDataFetched = (fetchedData) => {
+    setData(fetchedData)
+  }
+
   // 截斷過長的文字
   function truncateText(text, maxLength) {
     if (text.length <= maxLength) {
@@ -12,37 +22,45 @@ export default function Card({ title, brand, price, imagePath }) {
   }
 
   return (
-    <div className={`card h-auto ${styles['card']}`}>
-      <img
-        src={imagePath}
-        className={`card-img-top ${styles.cardImg}`}
-        alt="Product"
-      />
-      {/* 待完成，判斷有貨or缺貨(outofstock)，判斷是否為新品(ribbon) */}
-      <div className={styles['ribbon']}></div>
-      {/* <div className={styles['outofstock']}></div> */}
-      <div className="card-body w-100 position-relative">
-        <h4 className="card-title">{title}</h4>
-        <h6 className="card-title text-black-50">{brand}</h6>
-        <h5 className="card-title">$ {price}</h5>
-        <button
-          className={`d-sm-none d-block border border-2 border-primary rounded-circle ${styles['cartBtn']}`}
-        >
-          <i
-            className="fa-solid fa-cart-shopping fa-xl"
-            style={{ color: '#F1D6AB' }}
-          ></i>
-        </button>
-      </div>
-      <div className={`d-flex flex-column gap-3 ${styles['info']}`}>
-        <button className={styles['infoBtn1']}>加入購物車</button>
-        <button className={styles['infoBtn2']}>
-          <i className="fa-regular fa-heart"></i> Like
-        </button>
-        <button className={styles['infoBtn3']}>
-          <i className="fa-solid fa-angles-right"></i> Learn More
-        </button>
-      </div>
-    </div>
+    <>
+      {data && data.products && data.products.length > 0 ? (
+        <div className={`card h-auto ${styles['card']}`}>
+          <img
+            src={imagePath}
+            className={`card-img-top ${styles.cardImg}`}
+            alt="Product"
+          />
+          {/* 待完成，判斷有貨or缺貨(outofstock)，判斷是否為新品(ribbon) */}
+          <div className={styles['ribbon']}></div>
+          {/* <div className={styles['outofstock']}></div> */}
+          <div className="card-body w-100 position-relative">
+            <h4 className="card-title">{title}</h4>
+            <h6 className="card-title text-black-50">{brand}</h6>
+            <h5 className="card-title">$ {price}</h5>
+            <button
+              className={`d-sm-none d-block border border-2 border-primary rounded-circle ${styles['cartBtn']}`}
+            >
+              <i
+                className="fa-solid fa-cart-shopping fa-xl"
+                style={{ color: '#F1D6AB' }}
+              ></i>
+            </button>
+          </div>
+          <div className={`d-flex flex-column gap-3 ${styles['info']}`}>
+            <button className={styles['infoBtn1']}>加入購物車</button>
+            <button className={styles['infoBtn2']}>
+              <i className="fa-regular fa-heart"></i> Like
+            </button>
+            <button className={styles['infoBtn3']}>
+              <i className="fa-solid fa-angles-right"></i> Learn More
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="mt-5 mx-auto fs-3">課程籌備中,請敬請期待</div>
+      )}
+      {/*onDataFetched 為第12行定義的 這邊存入onCourseFetched並帶到子元件*/}
+      <ProductFetcher onProductFetched={onDataFetched} />
+    </>
   )
 }
---
+++ next-client/components/CardHover/ProductFetcher.js
@@ -0,0 +1,17 @@
+import { useEffect } from 'react'
+import axios from 'axios'
+
+export default function ProductFetcher({ onProductFetched }) {
+  useEffect(() => {
+    const FetchedProduct = async () => {
+      try {
+        const response = await axios.get('http://localhost:3005/api/products')
+        console.log(response.data)
+        onProductFetched(response.data)
+      } catch (error) {
+        console.log('資料獲取失敗：', error)
+      }
+    }
+    FetchedProduct()
+  }, [])
+}
