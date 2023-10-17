5d41229680a8eb9d514e5b5da03a484bc86963b1
Larry <trumpet.0417@gmail.com>
2023-10-17 21:01:04 +0800

    1017 2100

--- pages/product/index.js
+++ pages/product/index.js
@@ -18,7 +18,7 @@ export async function getStaticProps() {
 }
 
 export default function ProductIndex() {
-  // 從篩選子元件AsideFilter(component)傳值過來
+  // 儲存從篩選子元件AsideFilter(component)傳值過來
   const [filterProduct, setFilterProduct] = useState(0)
 
   // Drawer相關
@@ -77,9 +77,65 @@ export default function ProductIndex() {
       })
   }
 
+  // 篩選用
+  const [priceRange, setPriceRange] = useState({ min: 10, max: 30000 }) //數字物件
+  // 最後得到的項目
+  const [itemTotal, setItemTotal] = useState(0)
+  const [items, setItems] = useState([])
+  const getProductsQs = async (params, toFirstPage = false) => {
+    // 跳至第一頁
+    // 當重新過濾或重置選項，因重新載入資料需要跳至第一頁
+    // if (toFirstPage) {
+    //   setPage(1)
+    // }
+
+    // 用URLSearchParams產生查詢字串
+    const searchParams = new URLSearchParams(params)
+    const url = `http://localhost:3005/api/products/qs?${searchParams.toString()}`
+    const res = await axios.get(url)
+
+    if (Array.isArray(res.data.data)) {
+      // 設定獲取頁數總合
+      setItemTotal(res.data.total)
+      // 設定獲取項目
+      setItems(res.data)
+      //   console.log('res.data這是啥', res.data)
+      setFilterProduct(items)
+    }
+  }
+  useEffect(() => {
+    if (router.isReady) {
+      // 從router.query得到所有查詢字串參數
+      const { page, keyword, cat_ids, orderby, perpage, price_range } =
+        router.query
+
+      //   console.log(router.query)
+
+      // 設定回所有狀態(注意資料類型，所有從查詢字串來都是字串類型)
+      //   setPage(Number(page) || 1)
+      //   setKeyword(keyword || '')
+      //   setCatIds(cat_ids ? cat_ids.split(',').map((v) => Number(v)) : [])
+      //   setOrderby(orderby || 'id,asc')
+      //   setPerpage(Number(perpage) || 10)
+      setPriceRange(
+        price_range
+          ? {
+              min: Number(price_range.split(',')[0]),
+              max: Number(price_range.split(',')[1]),
+            }
+          : {
+              min: 10,
+              max: 30000,
+            }
+      )
+
+      // 載入資料
+      getProductsQs(router.query)
+    }
+  }, [router.query])
+
   return (
     <>
-      {/* <h1>{JSON.stringify(filterProduct)}</h1> */}
       <div className={styles.banner}>
         <div className="w-100 h-100 p-4 p-sm-0">
           <img
@@ -95,10 +151,93 @@ export default function ProductIndex() {
           <div className="d-none d-sm-block col-12 col-sm-3 pe-md-5 pe-1">
             <Accordion />
             <hr className="text-primary opacity-100"></hr>
-            <AsideFilter
+            {/* <AsideFilter
               filterProduct={filterProduct}
               setFilterProduct={setFilterProduct}
-            />
+            /> */}
+            {/* AsideFilter，篩選 */}
+            <div className="p-4 pt-0">
+              <div className="mb-2 fs-5">
+                <i className="fa-solid fa-filter"></i> 條件篩選
+              </div>
+              <div className="d-flex flex-column gap-1">
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="flexCheckDefault"
+                  />
+                  有貨
+                </div>
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="flexCheckDefault"
+                  />
+                  品牌
+                </div>
+                <hr className="opacity-75"></hr>
+                <div className="mb-2 fs-5">
+                  <i className="fa-solid fa-dollar-sign"></i> 價錢範圍
+                </div>
+                <div className="mb-3 d-flex justify-content-center align-items-center">
+                  <input
+                    type="number"
+                    className="col-5"
+                    min="0"
+                    value={priceRange.min}
+                    onChange={(e) => {
+                      setPriceRange({
+                        ...priceRange,
+                        min: Number(e.target.value),
+                      })
+                    }}
+                  ></input>
+                  <div className="col-2 fs-4 d-flex justify-content-center">
+                    ~
+                  </div>
+                  <input
+                    type="number"
+                    className="col-5"
+                    min="0"
+                    value={priceRange.max}
+                    onChange={(e) => {
+                      setPriceRange({
+                        ...priceRange,
+                        max: Number(e.target.value),
+                      })
+                    }}
+                  ></input>
+                </div>
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  style={{ width: '60%', margin: 'auto' }}
+                  onClick={() => {
+                    const params = {
+                      page: 1, // 跳至第一頁
+                      //   keyword,
+                      //   cat_ids: catIds.join(','),
+                      //   orderby,
+                      //   perpage,
+                      price_range: Object.values(priceRange).join(','),
+                    }
+
+                    router.push({
+                      pathname: router.pathname,
+                      query: params,
+                    })
+                    getProductsQs(params)
+                    console.log(priceRange)
+                  }}
+                >
+                  套用
+                </button>
+              </div>
+            </div>
           </div>
 
           {/* 排序 & card group */}
