a834d0de8c73e0bd52b3e4981e50e294b91254e9
jjjj88521 <jjjj88521@gmail.com>
2023-10-23 12:49:11 +0800

    評價RWD修BUG

--- components/product/ProductTab/review-tab/comment-item.js
+++ components/product/ProductTab/review-tab/comment-item.js
@@ -22,18 +22,21 @@ export default function CommentItem({
       <div className="col d-flex flex-column gap-4">
         <div className="d-flex justify-content-between w-100">
           <div className="vstack gap-2">
-            <h5 className="fw-bold text-primary mb-0">
-              {/* // 只保留 account 第一個與最後一個字母，中間每個字母替換成 *，有幾個字母就替換幾次 */}
-              {user_account}
-            </h5>
+            <div className="d-flex justify-content-between">
+              <h5 className="fw-bold text-primary mb-0">
+                {/* // 只保留 account 第一個與最後一個字母，中間每個字母替換成 *，有幾個字母就替換幾次 */}
+                {user_account.slice(0, 1)}*****{user_account.slice(-1)}
+              </h5>
+              <h6 className="text-secondary">{created_time}</h6>
+            </div>
+
             <Rate value={star} disabled style={{ fontSize: '14px' }} />
-            <div className="d-flex gap-2 text-secondary">
+            <div className="d-flex gap-2 text-secondary flex-wrap">
               {style.map((style, index) => (
                 <span key={index}>{style}</span>
               ))}
             </div>
           </div>
-          <h6 className="text-secondary">{created_time}</h6>
         </div>
         <div
           className="text-dark fs-6 fw-semibold lh-base"
--- pages/rent/[rt_id].js
+++ pages/rent/[rt_id].js
@@ -1,3 +1,4 @@
+import Card from '@/components/product/Card'
 import TabContainer from '@/components/product/ProductTab'
 import TabButton from '@/components/product/ProductTab/TabButton'
 import PdLoading from '@/components/product/pd-loading'
@@ -10,6 +11,9 @@ import { fetchProductLike, fetchRT } from '@/libs/productFetcher'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
+// swiper
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/scss'
 
 export default function RentDetail() {
   const { auth } = useAuth()
@@ -72,6 +76,45 @@ export default function RentDetail() {
             <TabButton tabName="spec">商品規格</TabButton>
             {/* <TabButton tabName="rt-desc">租用說明</TabButton> */}
           </TabContainer>
+          {/* 瀏覽過商品 */}
+          <section className="">
+            <div className="container border-top border-2 py-5">
+              <h2 className="fs-3 text-center text-secondary pb-4">
+                瀏覽過的商品
+              </h2>
+              <div className="px-4">
+                <Swiper
+                  spaceBetween={30}
+                  slidesPerView={1.5}
+                  breakpoints={{
+                    576: {
+                      slidesPerView: 4,
+                      spaceBetween: 50,
+                    },
+                  }}
+                  className="mySwiper"
+                >
+                  {recentlyViewed.map((product, index) => {
+                    const images = JSON.parse(product.images)
+                    return (
+                      <SwiperSlide key={index}>
+                        <Card
+                          id={product.id}
+                          cate={'rt'}
+                          title={product.name}
+                          brand={product.brand}
+                          price={product.price}
+                          image={`/images/product/${images[0]}`}
+                          link={`/rent/${product.id}`}
+                          stock={product.stock}
+                        />
+                      </SwiperSlide>
+                    )
+                  })}
+                </Swiper>
+              </div>
+            </div>
+          </section>
         </>
       )}
     </>
